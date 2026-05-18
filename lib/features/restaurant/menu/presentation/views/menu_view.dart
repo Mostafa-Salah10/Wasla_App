59495cf43502adb3b77ade10c9bd56e40d70cad5
Mostafa Salah <mostafasalahnabawy10@gmail.com>
2026-05-18 15:15:21 +0300

    simple refactor in menu

--- lib/features/restaurant/menu/presentation/views/menu_view.dart
+++ lib/features/restaurant/menu/presentation/views/menu_view.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wasla/core/config/localization/app_localizations.dart';
 import 'package:wasla/core/config/routes/app_routes.dart';
+import 'package:wasla/core/database/api/api_consumer.dart';
 import 'package:wasla/core/extensions/custom_navigator_extension.dart';
 import 'package:wasla/core/functions/get_user_id.dart';
+import 'package:wasla/core/service/service_locator.dart';
+import 'package:wasla/core/utils/app_colors.dart';
+import 'package:wasla/core/utils/app_sizes.dart';
+import 'package:wasla/core/utils/assets.dart';
 import 'package:wasla/core/widgets/bloc_status_handler.dart';
 import 'package:wasla/features/doctor_service/features/service/presentation/widgets/custom_doc_add_service_float_button.dart';
 import 'package:wasla/features/restaurant/menu/presentation/manager/cubit/resident_menu_cubit.dart';
 import 'package:wasla/features/restaurant/menu/presentation/widgets/menu_body.dart';
+import 'package:wasla/features/restaurant/orders/data/repo/orders_repo_impl.dart';
+import 'package:wasla/features/restaurant/orders/presentation/manager/cubit/orders_cubit.dart';
 
 class MenuView extends StatefulWidget {
   const MenuView({super.key});
@@ -38,7 +45,29 @@ class _MenuViewState extends State<MenuView> {
           );
         },
       ),
-      appBar: AppBar(title: Text('menu'.tr(context))),
+      appBar: AppBar(
+        title: Text('menu'.tr(context)),
+        actions: [
+          BlocProvider(
+            create: (context) =>
+                OrdersCubit(OrdersRepoImpl(api: sl<ApiConsumer>())),
+            child: Padding(
+              padding: EdgeInsets.only(right: AppSizes.marginDefault),
+              child: IconButton(
+                icon: Image.asset(
+                  Assets.assetsImagesTakeAway,
+                  color: AppColors.primaryColor,
+                  height: 25,
+                  width: 25,
+                ),
+                onPressed: () {
+                  context.pushScreen(AppRoutes.orderScreen);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
       body: BlocStatusHandler<ResidentMenuCubit, ResidentMenuState>(
         body: const MenuBody(),
         onRetry: () {
--- lib/features/restaurant/orders/presentation/views/reservation_view.dart
+++ lib/features/restaurant/orders/presentation/views/reservation_view.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wasla/core/config/localization/app_localizations.dart';
-import 'package:wasla/core/config/routes/app_routes.dart';
-import 'package:wasla/core/extensions/custom_navigator_extension.dart';
-import 'package:wasla/core/utils/app_colors.dart';
-import 'package:wasla/core/utils/app_sizes.dart';
-import 'package:wasla/core/utils/assets.dart';
+
 import 'package:wasla/core/widgets/bloc_status_handler.dart';
 import 'package:wasla/features/restaurant/orders/presentation/manager/cubit/orders_cubit.dart';
 import 'package:wasla/features/restaurant/orders/presentation/widgets/reservations/reservation_body.dart';
@@ -27,25 +23,7 @@ class _OrdersViewState extends State<ReservationView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('reservation'.tr(context)),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: AppSizes.marginDefault),
-            child: IconButton(
-              icon: Image.asset(
-                Assets.assetsImagesTakeAway,
-                color: AppColors.primaryColor,
-                height: 25,
-                width: 25,
-              ),
-              onPressed: () {
-                context.pushScreen(AppRoutes.orderScreen);
-              },
-            ),
-          ),
-        ],
-      ),
+      appBar: AppBar(title: Text('reservation'.tr(context))),
       body: BlocStatusHandler<OrdersCubit, OrdersState>(
         body: const ReservationBody(),
         onRetry: () {
